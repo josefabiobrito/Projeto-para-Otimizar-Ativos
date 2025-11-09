e3bfe2d90a974ba0dbe63e9c2ea1e173b8edf0a4
Jose-Brito1 <jose.brito1@usp.br>
2025-11-09 20:18:41 -0300

    Alteração decimal do botão de retorno

--- Dashboard_ONL.py
+++ Dashboard_ONL.py
@@ -238,7 +238,7 @@ else:
     dados = get_dados(selecionadas)
     info_ativos = get_metricas(dados)
     with st.form(key='meu_formulario'):
-        ret_alvo = st.number_input("Retorno Alvo (entre 0 e 1):", min_value=0.0, max_value=1.0, value=0.1, step=0.01, format="%.4f")
+        ret_alvo = st.number_input("Retorno Alvo (entre 0 e 1):", min_value=0.0, max_value=1.0, value=0.1, step=0.01, format="%.3f")
         submit_button = st.form_submit_button(label='Rodar')
         
     st.markdown("---")
